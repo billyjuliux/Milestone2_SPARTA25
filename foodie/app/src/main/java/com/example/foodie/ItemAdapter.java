108eb7c9a2d456664aabd186e4326231e6adabb8
billyjuliux <billyjulius01@gmail.com>
2020-08-06 15:04:15 +0700

    start over here

---
+++ foodie/app/src/main/java/com/example/foodie/ItemAdapter.java
@@ -0,0 +1,55 @@
+package com.example.foodie;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class ItemAdapter extends BaseAdapter {
+
+    LayoutInflater mInflater;
+    ListView yourfoodListView;
+    String[] foodNames, foodTimes;
+
+    public ItemAdapter (Context c, String[] n, String[] t){
+        foodNames = n;
+        foodTimes = t;
+        mInflater = (LayoutInflater) c.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return foodNames.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return foodNames[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+
+        View v = mInflater.inflate(R.layout.listview_detail, null);
+        TextView namesTV = (TextView) v.findViewById(R.id.namesTV);
+        TextView timesTV = (TextView) v.findViewById(R.id.timesTV);
+
+        String name = foodNames[i];
+        String time = foodTimes[1];
+
+        namesTV.setText(name);
+        timesTV.setText(time);
+
+
+        return v;
+    }
+}
--- foodie/app/src/main/java/com/example/foodie/MainActivity.java
+++ foodie/app/src/main/java/com/example/foodie/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.foodie;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView yourfoodListView;
+    String[] foodNames, foodTimes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        yourfoodListView = (ListView) findViewById(R.id.yourfoodListView);
+        foodNames = res.getStringArray(R.array.foodNames);
+        foodTimes = res.getStringArray(R.array.foodTimes);
+
+        ItemAdapter itemAdapter= new ItemAdapter(this, foodNames, foodTimes);
+        yourfoodListView.setAdapter(itemAdapter);
+
     }
 }
