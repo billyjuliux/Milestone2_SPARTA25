847b37df6a3a30ed492b805d7db0b2e211092c76
graciellavl <graciellavl@users.noreply.github.com>
2020-08-07 21:43:09 +0700

    fix recipes

--- foodie/app/src/main/java/com/example/foodie/Recipes.java
+++ foodie/app/src/main/java/com/example/foodie/Recipes.java
@@ -1,4 +1,15 @@
 package com.example.foodie;
 
-public class Recipes {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Recipes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipes);
+    }
+
 }
