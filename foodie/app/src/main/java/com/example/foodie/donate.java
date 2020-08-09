b41c46e47c5693c6a936067840c404d8f8405a38
cyn-rus <cyn.rusadi@gmail.com>
2020-08-09 21:13:29 +0700

    nambah spinner dulu bentar

--- foodie/app/src/main/java/com/example/foodie/donate.java
+++ foodie/app/src/main/java/com/example/foodie/donate.java
@@ -1,6 +1,5 @@
 package com.example.foodie;
 
-//import androidx.appcompat.app.AppCompatActivity;
 //import java.util.ArrayList;
 //import java.util.List;
 import android.app.Activity;
@@ -8,44 +7,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 //import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 public class donate extends AppCompatActivity {
 
-    private Spinner choose_loc;
+    private Spinner choose_loc, choose_food;
     private Button btnDonate;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.donate);
-
-        addListenerOnButton();
+        
         addListenerOnSpinnerItemSelection();
     }
 
     public void addListenerOnSpinnerItemSelection() {
         choose_loc = (Spinner) findViewById(R.id.donate_spinner);
+        choose_food = (Spinner) findViewById(R.id.food_spinner);
         choose_loc.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-    }
-
-    //get the selected dropdown list value
-    public void addListenerOnButton() {
-        choose_loc = (Spinner) findViewById(R.id.donate_spinner);
-        btnDonate = (Button) findViewById(R.id.submit_donate);
-
-        btnDonate.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(donate.this,
-                        "OnClickListener : " +
-                                "\ndonate_spinner : "+ String.valueOf(choose_loc.getSelectedItem()),
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
+        choose_food.setOnItemSelectedListener(new CustomOnItemSelectedListener());
     }
 }
